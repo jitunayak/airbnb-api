e21b726a1927a22b1a392b3b7868484ca553f92b
jitunayak <jitunayak715@gmail.com>
2024-04-15 14:22:57 +0530

    added dot env cloud setup

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,6 +3,6 @@ import { load } from 'https://deno.land/std@0.222.1/dotenv/mod.ts';
 const env = await load();
 
 export const config = {
-	DATABASE_URL: env['DATABASE_URL'] || Deno.env.get('DATABASE_URL'),
-	EMAIL_API_KEY: env['EMAIL_API_KEY'] || Deno.env.get('EMAIL_API_KEY'),
+	DATABASE_URL: env['DATABASE_URL'] || Deno.env.get('DATABASE_URL') || '',
+	EMAIL_API_KEY: env['EMAIL_API_KEY'] || Deno.env.get('EMAIL_API_KEY') || '',
 };
