f588c9d98a77e2350d19d6f217ad02edb47ec311
jitunayak <jitunayak715@gmail.com>
2024-04-17 01:04:54 +0530

    updated deno env variables

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,23 +1,24 @@
-import { load } from 'https://deno.land/std@0.222.1/dotenv/mod.ts';
+import { config } from 'npm:dotenv';
+config();
 
-const envFile = await load();
+interface Env {
+	DATABASE_URL: string;
+	EMAIL_API_KEY: string;
+	AUTH_SECRET: string;
+	KINDE_SECRET: string;
+	KINDE_CLIENT_ID: string;
+	KINDE_DOMAIN: string;
+	UPSTASH_TOKEN: string;
+	UPSTASH_URL: string;
+}
 
-Deno.env.set('DATABASE_URL', envFile['DATABASE_URL']);
-Deno.env.set('EMAIL_API_KEY', envFile['EMAIL_API_KEY']);
-Deno.env.set('AUTH_SECRET', envFile['AUTH_SECRET']);
-Deno.env.set('KINDE_SECRET', envFile['KINDE_SECRET']);
-Deno.env.set('KINDE_CLIENT_ID', envFile['KINDE_CLIENT_ID']);
-Deno.env.set('KINDE_DOMAIN', envFile['KINDE_DOMAIN']);
-Deno.env.set('UPSTASH_TOKEN', envFile['UPSTASH_TOKEN']);
-Deno.env.set('UPSTASH_URL', envFile['UPSTASH_URL']);
-
-export const env = {
-	DATABASE_URL: Deno.env.get('DATABASE_URL') || '',
-	EMAIL_API_KEY: Deno.env.get('EMAIL_API_KEY') || '',
-	AUTH_SECRET: Deno.env.get('AUTH_SECRET') || '',
-	KINDE_SECRET: Deno.env.get('KINDE_SECRET') || '',
-	KINDE_CLIENT_ID: Deno.env.get('KINDE_CLIENT_ID') || '',
-	KINDE_DOMAIN: Deno.env.get('KINDE_DOMAIN') || '',
-	UPSTASH_TOKEN: Deno.env.get('UPSTASH_TOKEN') || '',
-	UPSTASH_URL: Deno.env.get('UPSTASH_URL') || '',
+export const env: Env = {
+	DATABASE_URL: Deno.env.get('DATABASE_URL') as string,
+	EMAIL_API_KEY: Deno.env.get('EMAIL_API_KEY') as string,
+	AUTH_SECRET: Deno.env.get('AUTH_SECRET') as string,
+	KINDE_SECRET: Deno.env.get('KINDE_SECRET') as string,
+	KINDE_CLIENT_ID: Deno.env.get('KINDE_CLIENT_ID') as string,
+	KINDE_DOMAIN: Deno.env.get('KINDE_DOMAIN') as string,
+	UPSTASH_TOKEN: Deno.env.get('UPSTASH_TOKEN') as string,
+	UPSTASH_URL: Deno.env.get('UPSTASH_URL') as string,
 };
