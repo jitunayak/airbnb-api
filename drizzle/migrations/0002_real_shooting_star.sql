4e3f33354d3b96906f79b75c24af042456f45ec1
jitunayak <jitunayak715@gmail.com>
2024-05-30 12:20:46 +0530

    onboarding email supports checkin check out dates

---
+++ drizzle/migrations/0002_real_shooting_star.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "users" RENAME COLUMN "picture_url" TO "picture";
--- src/email/bookingConfirmation.ts
+++ src/email/bookingConfirmation.ts
@@ -1,16 +1,21 @@
+import React from 'react';
 import { Resend } from 'resend';
-// import BookingConfirmed from './templates/BookingConfirmed';
+import { BookingConfirmed } from './templates/BookingConfirmed';
 
 export const sendBookingConfirmationEmail = ({
 	to,
 	name,
 	apiKey,
 	bookingId,
+	checkInDate,
+	checkOutDate,
 }: {
 	to: string;
 	name: string;
 	apiKey: string;
 	bookingId: string;
+	checkInDate: string;
+	checkOutDate: string;
 }) => {
 	const resend = new Resend(apiKey);
 
@@ -18,7 +23,13 @@ export const sendBookingConfirmationEmail = ({
 		from: 'onboarding@resend.dev',
 		to: to,
 		subject: 'Airbnb booking confirmation',
-		html: `Your booking has been confirmed with aircnc. We wait to onboard you soon!`,
-		// react: BookingConfirmed({ name: name, url: 'https://aircnc-jitunayak.vercel.app', bookingId: bookingId }),
+		// html: `Your booking has been confirmed with aircnc. We wait to onboard you soon!`,
+		react: React.createElement(BookingConfirmed, {
+			name: name,
+			url: 'https://aircnc-jitunayak.vercel.app',
+			bookingId: bookingId,
+			checkInDate: checkInDate,
+			checkOutDate: checkOutDate,
+		}),
 	});
 };
